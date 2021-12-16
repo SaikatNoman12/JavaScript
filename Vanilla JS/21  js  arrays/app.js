3a3b4179edf4cfc9d8e978131b466bc26cffd664
Nomaan240 <noommaa240@gmail.com>
2021-12-16 13:56:25 +0600

    added js array

--- Vanilla JS/21  js  arrays/app.js
+++ Vanilla JS/21  js  arrays/app.js
@@ -89,6 +89,7 @@ console.log(a_);        // return ['noman', 'mahi', 'nadim', 'riodom']. data typ
 // Arrays are Object:====
 console.log("Arrays are Object:");
 
+
 // array access=
 let b_ = ['noman', 'mahadee', 'nirov', 'rakin'];
 
@@ -120,8 +121,133 @@ let e = 10000;      // data type number
 
 let r = [q , e , [10 , 20 , 30] , {name:"ali" , class:10}];
 
-console.log(r);     // return [q , e , f , [10 , 20 , 30] , {name:"ali" , class:10}]. JavaScript supports arrays, objects and various types of data within arrays.
+console.log(r);     // return [q , e , f , [10 , 20 , 30] , {name:"ali" , class:10}]. JavaScript supports arrays, objects, functions and various types of data within arrays.
 
+
+
+
+// Array propertise and method:====
+console.log("Array propertise and method:");
+
+let t = ['noman', 'mahadee', 'rakib', 'muhit'];
+
+// propertise
+let q_ = t.length;      // length is a propertise. 
+
+console.log(q_);        // return 4.  data ype number
+
+// method
+let w_ = t.sort();      // sort() is a method. Decorate the elements in the array with alpavet shape.
+
+console.log(w_);    // return ['mahadee', 'muhit', 'noman', 'rakib']. Data type object.
+
+
+
+
+// Accesing the last array element:====
+console.log("Accesing the last array element:");
+
+let u = ['mabi', 'rahi', 'kari', 'jubayer'];
+
+let p = u[u.length - 1];    // In this way accesing the last array element.
+
+console.log(p)      // retunr jubayer. data type string.
+
+
+
+
+// Looping Array Elements:====
+console.log("Looping Array Elements:");
+
+let fruit = ['Banna', 'Apple', 'Pinaapple', 'jackfruiit'];  // this is array.
+
+let con = fruit.length;        // loop condition
+
+
+// loop
+for( a = 0; a < con; a++){
+
+    console.log(a)      // return index number. data type number.
+
+    console.log( fruit[a]);      // return arrays element list. data type string
+
+};
+
+/*
+1. a = 0 means the loop starts in 0 number element in this array. 
+2.  a means arrays index.
+3. a < con means condition.
+4.  a++ means 0+1 ,1+1,2+1 .....etc. end last number index. 
+5. fruit[a] means index element. 
+*/
+
+
+
+
+let text = '<ul>';      // ul start tag.
+
+
+// loop
+for(i = 0; i < con; i++){
+    text += "<li>" + i + fruit[i] + "</li>";
+};
+
+text += "</ul>";    // ul close tag.
+
+document.getElementById('demo').innerHTML = text;       // return un order list .data type string.
+
+/*
+1. i = 0 means the loop starts in 0 number element in this array. 
+2. i means arrays index.
+3. i < con means condition.
+4. i++ means 0+1 ,1+1,2+1 .....etc. end last number index. 
+5. fruit[i] means index element. 
+*/
+
+
+
+
+// using forEach() method. forEach() use for all arrays in this type.=
+
+let t_ = ['nom', 'mahedi', 'rahim', 'feni'];
+
+t_.forEach(myFunction);     //  called forEach() method.
+
+function myFunction(value){
+    console.log(value);         // return a list nom mahedi rahim feni. data type string.
+};
+
+
+
+
+// Adding Array Elements:====
+console.log("Adding Array Elements:");
+
+
+// use push() method and add new array elements=
+let h = ['noman', 'rahim', 'karim'];
+
+h.push("ADDTheNewString");      // push() method added new element in array.
+ 
+console.log(h);     // rerurn ['noman', 'rahim', 'karim', 'ADDTheNewString']. data type object.
+
+
+// use length property and add new array elements=
+let p_ = ['noman', 'mahi', 'rahim', 'muhit'];
+
+p_[p_.length] = "nadim";    // length property added new element in array.
+
+console.log(p_);    // return ['noman', 'mahi', 'rahim', 'muhit', 'nadim']. data type object.
+
+
+// undefined element=
+let j = ['noma', 'rahim', 'iraz'];
+
+j[4] = "mahin";     // added mahin. 4 number index in array.
+
+console.log(j) ;     // return ['noma', 'rahim', 'iraz', empty, 'mahin']. data type object.
+
+console.log(j[3]);      // rerturn undefined. data type undefined.
 
 
 
