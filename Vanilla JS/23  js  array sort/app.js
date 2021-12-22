71d5ed34c5635d60cf6f97015349db6f5797b804
Nomaan240 <noommaa240@gmail.com>
2021-12-22 17:49:09 +0600

    add array sort

--- Vanilla JS/23  js  array sort/app.js
+++ Vanilla JS/23  js  array sort/app.js
@@ -152,14 +152,113 @@ console.log( "highest number is = ", o[0]);     // return hightest number is = 1
 
 
 
-// Using Math.max() on an Array:====
+// Using Math.max.apply() method on an Array:====
 console.log("Using Math.max() on an Array:");
 
-let p = [];
+let p = [1, 5, 7, 3, 20, 10];
+
+function nameHere(arr){
+    return Math.max.apply(null, arr);           // You can use Math.max.apply to find the highest number in an array.
+};
+
+console.log(nameHere(p));       // return 20. The highest number is 20.  Math.max.apply(null, [1, 2, 3]) is equivalent to Math.max(1, 2, 3).
+
+
+// It can also be used in this way.
+
+console.log(Math.max.apply(null, p));       // return 20. The highest number is 20. 
 
 
 
 
+//  using Math.min.apply() method an array:====
+console.log("using Math.min.apply() methos an array:");
+
+let d = [5, 7, 8, 1, 9, 2];
+
+function minHere(arry){
+    return Math.min.apply(null, arry);
+};
+
+console.log(minHere(d));        // return 1. The lowest number is 1. Mathmin.apply(null , [1, 2, 3]) is equivalent  to Math.min(1, 2, 3).
+
+
+// It can also be used in this way.
+console.log(Math.min.apply(null, d));       // return 1. The lowest number is 1
+
+
+
+
+// My Max/Min JavaScript Method:====
+console.log("My Max/Min JavaScript method:");
+
+
+// max:=
+console.log("Max");                 // working
+
+
+// min:=
+console.log("Min");                 // working
+
+
+
+
+// Sorting Object arrays:====
+console.log("sorting object arrys:");
+
+let g = [
+    {name:"noman", year:2002},
+    {name:"nahid", year:2017},
+    {name:"rakib", year:2012},          // this is oject arrays.
+    {name:"kabir", year:2005},
+    {name:"sokina", year:2009}
+];
+
+/* use sort method. */
+g.sort(function(a, b){
+    return a.year - b.year;     // sort() method use an object year property.
+});
+
+console.log(g);        
+/*
+return=
+0: {name: 'noman', year: 2002}
+1: {name: 'kabir', year: 2005}
+2: {name: 'sokina', year: 2009}         //    you can sorting an object arrays. 
+3: {name: 'rakib', year: 2012}
+4: {name: 'nahid', year: 2017}
+*/
+
+
+
+
+// Comparing string properties is a little more complex:====
+console.log("Comparing string properties is a little more complex:");
+
+
+let cars = [
+    {type:"Volvo", year:2016},
+    {type:"Saab", year:2001},
+    {type:"BMW", year:2010}
+  ];
+
+cars.sort(function(a, b){
+
+    let x = a.type.toLowerCase();
+    let y = b.type.toLowerCase();
+
+    if (x < y) {return -1;}         // (x < y) it is codition in if. It's return -1.
+    if (x > y) {return 1;}          // (x > y) it is condition in if. It;s retrun 1.
+    return 0;
+  
+});
+
+console.log(cars);
+/* return
+0: {type: 'BMW', year: 2010}
+1: {type: 'Saab', year: 2001}       // length 3.
+2: {type: 'Volvo', year: 2016}
+*/
 
 
 
