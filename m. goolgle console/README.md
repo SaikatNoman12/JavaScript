a4a54fedf45c9be706a8301c79842270640f22c0
saikatnoman12 <noommaa240@gmail.com>
2022-10-13 21:13:38 +0600

    add google console

---
+++ m. goolgle console/README.md
@@ -0,0 +1,31 @@
+# JavaScript Console
+
+## All js console:
+
+    console.log();
+
+    console.table(array/object);
+
+    console.error();
+
+    console.warn();
+
+
+    // this is write serially;
+    console.time('sameText');
+
+    //another console;
+    
+    console.timeLog('sameText');
+
+    //another console
+    
+    console.timeStamp('sameText');
+
+    //another console
+    
+    console.timeEnd('sameText');
+
+
+
+    console.clear();
---
+++ m. goolgle console/app.js
@@ -0,0 +1,37 @@
+
+
+// use console.log();
+console.log('Hello My good fried!');
+
+
+// use console.table(array);
+console.table([1, 2, 3, 4, 5, 6]);
+
+
+// use console.error();
+console.error('Something is bad. Go back, Go back!');
+
+
+// use console.warn();
+console.warn('Last warning for you!');
+
+
+// use console.time() , console.timeLog(), console.timeStamp() & console.timeEnd();
+console.time('Test');
+
+console.warn('Go back! get out.');
+console.warn('Go back! get out.');
+
+console.timeLog('Test');
+
+console.warn('Go back! get out.');
+console.warn('Go back! get out.');
+console.warn('Go back! get out.');
+
+console.timeEnd('Test');  // get execute time;
+
+console.ti
+
+// use console.clear();
+// console.clear();
+
