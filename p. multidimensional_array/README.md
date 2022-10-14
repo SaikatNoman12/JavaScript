4cac98b209a95e035b5a2510bafd62518fd97568
saikatnoman12 <noommaa240@gmail.com>
2022-10-14 22:26:24 +0600

    javaScript multidimentsional array

---
+++ p. multidimensional_array/README.md
@@ -0,0 +1,13 @@
+# JavaScript MultiDimensional Array;
+
+## Array looks like
+
+    const array = [
+
+        ['Name', 'Age', 'Degree', 'Job'],
+        ['Deo', 25, 'BSC', 'Jr. Software Engineer'],
+        ['Jhone', 28, 'BBA', 'Sr. Manager'],
+        ['Prianka', 49, 'BSC', 'Sr. Actor'],
+        ['Dipika', 37, 'IBA', 'Sr. Actor']
+
+    ];
---
+++ p. multidimensional_array/app.js
@@ -0,0 +1,36 @@
+/* === JavaScript Multidimensional Array === */
+
+
+// use multidimensional array;
+const array = [
+    ['Name', 'Age', 'Degree', 'Job'],
+    ['Deo', 25, 'BSC', 'Jr. Software Engineer'],
+    ['Jhone', 28, 'BBA', 'Sr. Manager'],
+    ['Prianka', 49, 'BSC', 'Sr. Actor'],
+    ['Dipika', 37, 'IBA', 'Sr. Actor']
+]
+
+const arrayDataShow = (array) =>
+    (function (myArr) {
+        const table = document.querySelector('#table');
+
+        for (let i = 0; i < myArr.length; i++) {
+            const createTr = document.createElement('tr');
+
+            for (let j = 0; j < myArr[i].length; j++) {
+
+                const createTd = document.createElement('td');
+                createTd.innerText = myArr[i][j];
+                createTr.appendChild(createTd);
+
+            }
+
+            table.appendChild(createTr);
+        }
+
+    })(array);
+
+arrayDataShow(array);
+
+
+
